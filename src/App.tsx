f57fbb1b099ab8aba1576c9da782f2fbc14f0823
Apollo_Dot <72624705+RiverDot@users.noreply.github.com>
2023-05-17 13:22:31 -0400

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center text-center m-0 pt-[10vh]">
+      
+    </div>
+  );
+}
+
+export default App;
